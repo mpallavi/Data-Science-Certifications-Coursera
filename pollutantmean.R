23159339be0ab522e36c636f0822b3e8d9190149
mpallavi <murthyp@indiana.edu>
2015-03-01 19:15:04 -0500

    adding corr.R file

---
+++ pollutantmean.R
@@ -0,0 +1,8 @@
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  data = lapply(id, function(i) read.csv(paste(directory, "/", formatC(i, 
+                                                                       width = 3, flag = "0"), ".csv", sep = ""))[[pollutant]])
+  
+  return(mean(unlist(data), na.rm = TRUE))
+}
+
